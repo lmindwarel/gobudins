c53d6e498427bc0e2550df3720da6b9efcf69488
antoine <mindware@protonmail.com>
2021-06-01 22:24:42 +0200

    added GetCurrentUserConnectionAccounts

--- accounts.go
+++ accounts.go
@@ -17,6 +17,10 @@ func (ctrl *Controller) GetAccounts(userID string, token string) (response Accou
 	return response, errors.Wrap(err, "failed to request budget insight api")
 }
 
+func (ctrl *Controller) GetCurrentUserConnectionAccounts(connectionID string, token string) (response AccountsResponse, err error) {
+	return ctrl.GetConnectionAccounts(UserMe, connectionID, token)
+}
+
 func (ctrl *Controller) GetConnectionAccounts(userID string, connectionID string, token string) (response AccountsResponse, err error) {
 	route := fmt.Sprintf("%s/%s/connections/%s%s", RouteUsers, userID, connectionID, RouteAccounts)
 	err = ctrl.request(http.MethodGet, route, nil, nil, token, &response)
