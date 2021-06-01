22689766bce9d103e72771773fef6a8d6d449bda
antoine <mindware@protonmail.com>
2021-06-01 21:00:36 +0200

    added Account disabled hook

--- webhooks.go
+++ webhooks.go
@@ -8,13 +8,15 @@ import (
 )
 
 type WebhooksListeners struct {
-	OnUserCreated   func(User)
-	OnAccountSynced func(SyncedAccount)
+	OnUserCreated     func(User)
+	OnAccountSynced   func(SyncedAccount)
+	OnAccountDisabled func(SyncedAccount)
 }
 
 func (ctrl *Controller) SetupRoutesGin(e *gin.RouterGroup) {
 	e.POST("/user/created", ctrl.whUserCreated)
 	e.POST("/accounts/synced", ctrl.whAccountSynced)
+	e.POST("/accounts/disabled", ctrl.whAccountDisabled)
 }
 
 func (ctrl *Controller) whUserCreated(c *gin.Context) {
@@ -37,4 +39,15 @@ func (ctrl *Controller) whAccountSynced(c *gin.Context) {
 	}
 
 	ctrl.listeners.OnAccountSynced(account)
+}
+
+func (ctrl *Controller) whAccountDisabled(c *gin.Context) {
+	var account SyncedAccount
+	if err := c.ShouldBindJSON(&account); err != nil {
+		fmt.Printf("Failed to unmarshal synced account: %s\n", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+
+	ctrl.listeners.OnAccountDisabled(account)
 }
